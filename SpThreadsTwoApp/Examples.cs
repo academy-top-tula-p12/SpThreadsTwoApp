31d2027571205bfde85580a43527217e823dd7ce
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-06-13 21:18:52 +0300

    Добавьте файлы проекта.

---
+++ SpThreadsTwoApp/Examples.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpThreadsTwoApp
+{
+    internal class Examples
+    {
+        static int count = 0;
+        public static void LockMutexEvents()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new(CountInc);
+                thread.Name = $"Thread {i + 1}";
+                thread.Start();
+            }
+        }
+
+        static void CountInc()
+        {
+
+            for (int i = 0; i < 100000; i++)
+            {
+                // LOCK
+                //lock (locker)
+                //{
+                //    count++;
+                //}
+
+                // MONITOR
+                //Monitor.Enter(locker);
+                //count++;
+                //Monitor.Exit(locker);
+
+                // MUTEXT
+                //mutex.WaitOne();
+                //count++;
+                //mutex.ReleaseMutex();
+
+                // RESET EVENT
+                //waitHandler.WaitOne();
+                //count++;
+                //waitHandler.Set();
+
+                //Console.WriteLine($"{Thread.CurrentThread.Name} - {count}");
+                //Thread.Sleep(1);
+            }
+            Console.WriteLine($"{Thread.CurrentThread.Name} - {count}");
+
+        }
+    }
+}
---
+++ SpThreadsTwoApp/Program.cs
@@ -0,0 +1,44 @@
+﻿//int count = 0;
+//object locker = new();
+//Mutex mutex = new();
+//AutoResetEvent waitHandler = new AutoResetEvent(true);
+
+for (int i = 0; i < 10; i++)
+{
+    Car car = new(i + 1);
+}
+    
+
+
+class Car
+{
+    static Semaphore semaphore = new(4, 4);
+    Thread carThread;
+    int count = 5;
+    static Random random = new Random();
+
+    public Car(int id)
+    {
+        carThread = new(Parking);
+        carThread.Name = $"Car {id}";
+        carThread.Start();
+    }
+
+    public void Parking()
+    {
+        while(count > 0)
+        {
+            semaphore.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name} IN parking");
+
+            Thread.Sleep(random.Next(100, 200));
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} OUT parking");
+            semaphore.Release();
+
+            count--;
+            Thread.Sleep(random.Next(200, 300));
+        }
+    }
+}
+
